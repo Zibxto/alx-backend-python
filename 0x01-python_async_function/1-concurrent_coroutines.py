6e35be2ca7a564ccc2819b5ec711a484b12df307
zibxto <christianaziba@gmail.com>
2024-03-18 23:28:53 +0100

    make file executable

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """An asynchronous coroutine"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A  coroutine that return the list of all the delays (float values).
     The list of the delays should be in ascending order
     without using sort() because of concurrency
     """
-    result_list: list[float] = []
+    result_list: List[float] = []
     for _ in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         result = await task
