a9a457174302cec801c06838ad93859ece1b4d10
zibxto <christianaziba@gmail.com>
2024-03-19 00:31:13 +0100

    solve task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> str:
+def task_wait_random(max_delay: int) -> object:
     """
     A  coroutine that takes an int and returns an asyncio.Task
     """
