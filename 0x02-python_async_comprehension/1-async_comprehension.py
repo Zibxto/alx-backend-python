12655987975ee39fbd72b145684ddb92c6f450ba
zibxto <christianaziba@gmail.com>
2024-03-19 19:17:44 +0100

    update solution

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """An asynchronous coroutine"""
-import asyncio
-import random
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:  # type: ignore
+async def async_comprehension() -> List[float]:  # type: ignore
     """
     The coroutine will collect 10 random numbers using
     an async comprehensing over async_generator,
