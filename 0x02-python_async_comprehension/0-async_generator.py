2236f7b3a885225b63977f3d6a71f65250bb45c8
zibxto <christianaziba@gmail.com>
2024-03-19 18:51:46 +0100

    update solution to use float random numbers

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """An asynchronous coroutine"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """
     The coroutine will loop 10 times,
     each time asynchronously wait 1 second,
@@ -12,4 +13,4 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield (random.uniform(0, 10))
+        yield random.uniform(0, 10)
